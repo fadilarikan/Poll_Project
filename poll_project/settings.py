e76cded2982d4dac15707eabf521b2223ed0e584
fadilarikan <fadilarikan19664@gmail.com>
2021-08-08 17:30:49 +0300

    heroku icin gerekli dosyalar eklendi

--- poll_project/settings.py
+++ poll_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-71e)v!l0r^htx!_u2-i6@h4bs@4g#lkdr^djkwfwob)sjoiq8y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fadilarikan-poll-project.herokuapp.com']
 
 
 # Application definition
